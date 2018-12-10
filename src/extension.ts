74f85ae79cb363ed7bff3ce18e89678af604f9e1
Scott Odle <sjo@asu.edu>
2018-12-10 11:24:24 -0700

    get mp3 from polly

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,14 @@
 'use strict';
+
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+
+interface IPollyResponse {
+    AudioStream: Buffer;
+}
+
+const editor = vscode.window.activeTextEditor;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -11,17 +18,36 @@ export function activate(context: vscode.ExtensionContext) {
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "chatterbox" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+    let chatterbox = vscode.commands.registerCommand('extension.chatterbox', () => {
+        if (editor !== undefined) {
+            let selection = editor.document.getText(editor.selection);
+            const aws = require('aws-sdk');
+            let conf = vscode.workspace.getConfiguration('chatterbox');
+            aws.config.credentials = new aws.SharedIniFileCredentials({
+                profile: conf.awsProfile
+            });
+            const polly = new aws.Polly({
+                region: conf.awsRegion
+            });
+            if (`"'`.includes(selection[0])) {
+                selection = selection.substring(1);
+            }
+            if (`"'`.includes(selection[selection.length - 1])) {
+                selection = selection.substring(0, selection.length - 2);
+            }
+            polly.synthesizeSpeech({
+                OutputFormat: 'mp3',
+                Text: selection,
+                TextType: (selection.includes('<speak>')) ? 'ssml' : 'text',
+                SampleRate: `8000`,
+                VoiceId: conf.pollyVoice
+            }, (err: Error, data: IPollyResponse) => {
+                
+            });
+        }
     });
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(chatterbox);
 }
 
 // this method is called when your extension is deactivated
